5cbb6db2c887b538ed60de0a1bbfe3e20cf9a882
Ian Salt <ian.salt@iris-worldwide.com>
2020-02-05 13:11:32 +0000

    useState to useReducer update

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,29 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useReducer } from 'react'
 //  create context object
 const BlogContext = createContext()
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post ${state.length + 1}` }]
+    default:
+      return state
+  }
+}
 
 // provider accepts information and provides it down
 // to reach deeply nested components
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([])
+  const [blogPosts, dispatch] = useReducer(blogReducer, [])
 
   const addBlogPost = () => {
-    setBlogPosts(
-      [...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]
-    )
+    dispatch({ type: 'add_blogpost' })
   }
 
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+    <BlogContext.Provider
+      value={{ blogData: blogPosts, addBlogPost }}
+    >
       {children}
     </BlogContext.Provider>
   )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,22 +1,27 @@
 import React, { useContext } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Button } from 'react-native'
 // import the context object
 import BlogContext from '../context/BlogContext'
 import styles from './styles/screenStyles'
 
 const IndexScreen = () => {
   const { h1 } = styles
-  const blogPosts = useContext(BlogContext)
+  const { blogData, addBlogPost } = useContext(BlogContext)
 
   return (
     <View>
-      <Text style={h1}>Index Screen</Text>
-    <FlatList
-      keyExtractor={(blogPost) => blogPost.title}
-      render={({ item }) =>{
-        return <Text>{item.title}</Text>
-      }}
-    />
+      <Text style={h1}>Index Screen: </Text>
+      <Button
+        title={'Add Post'}
+        onPress={addBlogPost}
+      />
+      <FlatList
+        data={blogData}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) =>{
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   )
 }
